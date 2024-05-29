a0be98ab5b1b9b1bb334c41b622bab226c255a47
Fabian <50114802+fabalexsie@users.noreply.github.com>
2024-05-29 23:08:24 +0200

    small changes

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <title>Upload your files</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
   <form enctype="multipart/form-data" action="index.php" method="POST">

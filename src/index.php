094725a84dc614825cc7c17f1d4da08fed9d6a28
Fabian <50114802+fabalexsie@users.noreply.github.com>
2024-05-31 01:35:36 +0200

    add PWA manifest.json

--- src/index.php
+++ src/index.php
@@ -19,6 +19,7 @@
     <title>Upload your files</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="color-scheme" content="light dark" />
+    <link rel="manifest" href="/manifest.json">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css" />
   </head>
   <body>
